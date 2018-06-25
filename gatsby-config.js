4378fe07a7ad0d35c01ec7fc72fc1f196fb8daeb
Clay Diffrient <clay.diffrient@gmail.com>
2018-06-24 21:26:21 -0600

    Remove typography.js
    This is an attempt to see if this causes less lag
    rendering stuff at initial render time.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,5 @@ module.exports = {
   siteMetadata: {
     title: "Learn a11y"
   },
-  plugins: [
-    "gatsby-plugin-react-helmet",
-    {
-      resolve: "gatsby-plugin-typography",
-      options: {
-        pathToConfigModule: `src/utils/typography.js`
-      }
-    }
-  ]
+  plugins: ["gatsby-plugin-react-helmet"]
 };
--- src/utils/typography.js
+++
@@ -1,7 +0,0 @@
-import Typography from "typography";
-
-import deYoung from "typography-theme-de-young";
-
-const typography = new Typography(deYoung);
-
-export default typography;
