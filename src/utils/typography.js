4483c9046ddcd837d25a26d408fe9ead0f8c0b57
Clay Diffrient <clay.diffrient@gmail.com>
2018-06-29 22:16:47 -0600

    Restructure landing page with css grid

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,16 @@
 import Typography from "typography";
+import variables from "./variables";
 
 import deYoung from "typography-theme-de-young";
+
+deYoung.bodyFontFamily = [
+  "LatoWeb",
+  "Lato",
+  "Helvetica Neue",
+  "Helvetica",
+  "Arial",
+  "sans-serif"
+];
 
 const typography = new Typography(deYoung);
 const { rhythm, scale } = typography;
