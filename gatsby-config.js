de22c60b431869c6d05d8ac986891a3703143aec
Clay Diffrient <clay.diffrient@gmail.com>
2018-07-03 17:24:25 -0600

    Add sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: "Learn a11y"
+    title: "Learn a11y",
+    siteUrl: "https://learna11y.org"
   },
   plugins: [
     "gatsby-plugin-glamor",
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-typography",
       options: {
